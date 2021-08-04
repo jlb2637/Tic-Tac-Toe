5daeba6aff23b8cf54a894acda7ff0394709f444
jlb2637 <47393028+jlb2637@users.noreply.github.com>
2021-08-04 16:52:17 -0400

    added general notes for game in main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,19 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World!");
+        /*general structure notes:
+        -this class will be where all the action happens
+            -use game class functions to 'play'
+        -a board class that contains all of the gama data
+                -probably a type of array
+                -possible graphics here
+        -a game class
+                -check for if anyone won, let whoevers turn it is play
+        -?? a player class
+                -create a player object to interact with the board
+                should contain things like make a move or
+                    end current players turn
+        */
     }
+
 }
